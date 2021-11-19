67538c896b1bb1bb18dfce8ccfa2730c8753ae28
EC2 Default User <ec2-user@ip-172-31-61-0.ec2.internal>
2021-11-19 07:19:59 +0000

    [Finish]Task1-3

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
 
   resources :books do
-
+  resources :book_comments, only: [:create, :destroy]
   resource :favorites, only: [:create, :destroy]
-
   end
 
 root to: 'homes#top'
